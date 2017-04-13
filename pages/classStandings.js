9640f1da21df9c4eb70521e43dbb76571e466cc4
Joakim Waltersson <Joakim.waltersson@gmail.com>
2017-04-13 21:22:40 +0200

    Extract mutable data to state

--- pages/classStandings.js
+++ pages/classStandings.js
@@ -4,16 +4,25 @@ import { Runner } from '../components/runner'
 import { StandingsTable } from '../components/standingsTable'
 
 export default class extends React.Component {
+    constructor() {
+        super()
+        this.state = { results: []}
+    }
     static async getInitialProps ({query}) {
         const compId = query.compId
         const className = query.className
-        const standings = await GetClassStandings({className: className, compId: compId})
-        return standings
+        return {compId: compId,
+            className: className
+        }
+    }
+    async componentDidMount() {
+        const standings = await GetClassStandings({className: this.props.className, compId: this.props.compId})
+        this.setState({results: standings.results})
     }
     render() {
         return <div>
             <h2>{this.props.url.query.className}</h2>
-            <StandingsTable results={ this.props.results } />
+            <StandingsTable results={ this.state.results } />
             </div>
     }
 }
